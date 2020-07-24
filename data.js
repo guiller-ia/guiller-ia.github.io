fc0144fa2d3dbd81ec29331bc451355c5d70ba68
Guiller IA <9ui11er.i4@gmail.com>
2020-07-23 23:00:37 -0400

    adding files

---
+++ data.js
@@ -0,0 +1,152 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-whatsapp-image-2020-07-23-at-84632-pm",
+      "name": "WhatsApp Image 2020-07-23 at 8.46.32 PM",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 320,
+      "initialViewParameters": {
+        "yaw": 0.3565607089623821,
+        "pitch": -0.04337339918152594,
+        "fov": 1.364816294245251
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.588706603564426,
+          "pitch": 0.0750122782099325,
+          "rotation": 3.141592653589793,
+          "target": "2-whatsapp-image-2020-07-23-at-84633-pm-1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.11880800916978274,
+          "pitch": -0.3048824965843586,
+          "title": "Carrera de Administración de empresas",
+          "text": "Predios de la carrea de administración de empresas"
+        },
+        {
+          "yaw": 0.9593066124674223,
+          "pitch": -0.12941089647072346,
+          "title": "Farmacia",
+          "text": "Farmacia universitaria<div><br></div>"
+        }
+      ]
+    },
+    {
+      "id": "1-whatsapp-image-2020-07-23-at-84632-pm-1",
+      "name": "WhatsApp Image 2020-07-23 at 8.46.32 PM (1)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 320,
+      "initialViewParameters": {
+        "yaw": -0.1802175707715783,
+        "pitch": -0.031807159399779295,
+        "fov": 1.364816294245251
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.0217972704781886,
+          "pitch": -0.015146524565940567,
+          "rotation": 4.71238898038469,
+          "target": "2-whatsapp-image-2020-07-23-at-84633-pm-1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.2396913217057115,
+          "pitch": -0.06276239039665477,
+          "title": "Carrera de matemática",
+          "text": "Oficinas de la carrera de mateaticas de la facultadad de ciencias puras y naturales de la UMSA"
+        },
+        {
+          "yaw": -0.8071109977763733,
+          "pitch": -0.03773862159968466,
+          "title": "Carrera de estadistica",
+          "text": "Oficinas de la carrera de estadistica de la facultad de ciencias puras y naurales"
+        }
+      ]
+    },
+    {
+      "id": "2-whatsapp-image-2020-07-23-at-84633-pm-1",
+      "name": "WhatsApp Image 2020-07-23 at 8.46.33 PM (1)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 320,
+      "initialViewParameters": {
+        "yaw": 2.146976835434627,
+        "pitch": -0.030881879139307955,
+        "fov": 1.364816294245251
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.364945086647312,
+          "pitch": -0.043633171162163364,
+          "rotation": 7.0685834705770345,
+          "target": "1-whatsapp-image-2020-07-23-at-84632-pm-1"
+        },
+        {
+          "yaw": 0.3049192869255357,
+          "pitch": -0.05645700856745961,
+          "rotation": 3.141592653589793,
+          "target": "0-whatsapp-image-2020-07-23-at-84632-pm"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.3101279740320315,
+          "pitch": -0.12325412956625392,
+          "title": "Entrada posterior del monoblock",
+          "text": "Text"
+        },
+        {
+          "yaw": 1.7326708992220237,
+          "pitch": -0.039242191553020334,
+          "title": "Vicedecanato de alguna facultad",
+          "text": "Text"
+        },
+        {
+          "yaw": 3.0906392638749596,
+          "pitch": -0.43911071952807745,
+          "title": "Prefacultativo FCPN",
+          "text": "Oficinas del prefacultativo de la facultad de ciencias puras y naturales"
+        }
+      ]
+    }
+  ],
+  "name": "adm",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
